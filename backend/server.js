6f9a0c09834fc50add439964c82a326f0229dfdd
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-04-14 14:41:11 +0200

    backed app

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,17 @@
-import express from 'express';
-import cors from 'cors';
+import express, { Router } from 'express';
 import chalk from 'chalk';
-import cookieParser from 'cookie-parser';
+
+import app from './app.js';
 
 import dbInit from './db/index.js';
 
-import { userRouter, bookRouter } from './routes/index.js';
-import { errorHandler, ErrorResponse } from './utils/index.js';
-import mongoose from 'mongoose';
-
-await dbInit();
-
-const app = express();
-
 const port = process.env.PORT || 8901;
-// app.use(cors());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5173', 'https://wdg21-personal-library.netlify.app'],
+dbInit()
+  .then(() => {
+    app.listen(port, () => console.log(chalk.bgGreen(` Library API listening on port ${port}... `)));
   })
-);
-
-app.use(express.json(), cookieParser());
-
-app.get('/', async (req, res) => {
-  const dbResponse = await mongoose.connection.db.admin().ping();
-  if (dbResponse.ok !== 1) throw new ErrorResponse('DB not connected', 503);
-  res.json({ message: 'Running', dbResponse });
-});
-
-app.use('/users', userRouter);
-app.use('/books', bookRouter);
-
-app.use('/*splat', (req, res) => {
-  throw new ErrorResponse(`Check the route. You used ${req.originalUrl}`, 404);
-});
-
-app.use(errorHandler);
-
-app.listen(port, () => console.log(chalk.bgGreen(` Library API listening on port ${port}... `)));
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
