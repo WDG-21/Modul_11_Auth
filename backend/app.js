a4ee0047539b3c3921bc4008b76c65bd2f284d44
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-04-14 14:43:01 +0200

    app backend

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+import express, { Router } from 'express';
+import { userRouter, bookRouter } from './routes/index.js';
+import { errorHandler, ErrorResponse } from './utils/index.js';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173', 'https://wdg21-personal-library.netlify.app'],
+  })
+);
+
+app.use(express.json(), cookieParser());
+
+app.get('/', async (req, res) => {
+  const dbResponse = await mongoose.connection.db.admin().ping();
+  if (dbResponse.ok !== 1) throw new ErrorResponse('DB not connected', 503);
+  res.json({ message: 'Running', dbResponse });
+});
+
+const v1Router = Router();
+
+v1Router.use('/users', userRouter);
+v1Router.use('/books', bookRouter);
+
+app.use('/api/v1', v1Router);
+
+app.use('/*splat', (req, res) => {
+  throw new ErrorResponse(`Check the route. You used ${req.originalUrl}`, 404);
+});
+
+app.use(errorHandler);
+
+export default app;
